64453aef6b1177ad5f928e2cefbb7d69e05fde52
BakaDuang <31074192+bakaduang@users.noreply.github.com>
2023-06-30 11:49:14 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,20 +2,19 @@
 from __future__ import annotations
 import asyncio
 import os
+from wechaty_puppet_itchat import PuppetItChat
+from wechaty_puppet import PuppetOptions
 from wechaty import Wechaty, WechatyOptions
 
-from dotenv import load_dotenv
 from wechaty_plugin_contrib.contrib.info_logger import InfoLoggerPlugin
 from src.plugins.ding_dong import DingDongPlugin
 from src.plugins.repeater import RepeaterPlugin
 from src.plugins.counter import CounterPlugin, UICounterPlugin
-from src.plugins.github_message_forwarder import GithubMessageForwarderPlugin
 
 
 if __name__ == "__main__":
-    load_dotenv()
     options = WechatyOptions(
-        port=os.environ.get('port', 8004)
+        puppet=PuppetItChat(PuppetOptions())
     )
     bot = Wechaty(options)
     bot.use([
@@ -24,8 +23,5 @@ if __name__ == "__main__":
         InfoLoggerPlugin(),
         CounterPlugin(),
         UICounterPlugin(),
-        GithubMessageForwarderPlugin(
-            endpoint=os.environ.get('github_endpoint', None) or "your-custom-endpoint"
-        ),
     ])
     asyncio.run(bot.start())
